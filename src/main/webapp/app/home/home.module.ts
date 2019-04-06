9b9dd651e071a83f671864000d436544e9484fb1
ahmedhajyahmed <hajyahmedboh@yahoo.com>
2019-04-07 00:49:49 +0200

    add role expert

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 
 import { AskAnExpertSharedModule } from 'app/shared';
 import { HOME_ROUTE, HomeComponent } from './';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
     imports: [AskAnExpertSharedModule, RouterModule.forChild([HOME_ROUTE])],
-    declarations: [HomeComponent],
+    declarations: [HomeComponent, SliderComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AskAnExpertHomeModule {}
---
+++ src/main/webapp/app/home/slider/slider.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'jhi-slider',
+    templateUrl: './slider.component.html',
+    styles: []
+})
+export class SliderComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
